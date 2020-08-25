a718fc96110b716d7ccd4253f2d0a85d09aa32ea
AgileDesign2 <agiledesign2.0@gmail.com>
2020-08-25 04:31:14 -0400

    update axios post request

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -22,14 +22,14 @@ const LoginPage = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try{
-      const response = await axiosInstance.post('/rest-auth/login/',{
+      const response = await axiosInstance.post('/rest-auth/login/', {
         username: 'dgomez5',
         email: email,
         password: password
       });
-      axiosInstance.defaults.headers['Authorization'] = "JWT" + response.data.key;
-      localStorage.setItem('access_token', response.data.key);
-      localStorage.setItem('refresh_token', response.data.refresh);
+      //axiosInstance.defaults.headers['Authorization'] = response.data.key;
+      localStorage.setItem('access_key', response.data.key);
+      //localStorage.setItem('refresh_token', response.data.refresh);
       setisLoggedIn(true);
     }catch(error){
       setisLoggedIn(false);
--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,35 +1,34 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({
+const CSRF_COOKIE_NAME = "csrftoken";
+const CSRF_HEADER_NAME = "X-CSRFToken";
+
+export default axios.create({
     baseURL: 'http://127.0.0.1:8000',
     timeout: 5000,
-    headers:{
-        'Authorization': 'JWT' + localStorage.getItem('access_token'),
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
-})
+    responseType: "json",
+    xsrfCookieName: CSRF_COOKIE_NAME,
+    xsrfHeaderName: CSRF_HEADER_NAME,
+});
 
-axiosInstance.interceptors.response.use(response => response,
+axios.interceptors.response.use(response => response,
     error => {
-        const originalRequest = error.config;
-        if (error.response.status === 401 && error.response.statusText === "Unauthorized"){
+        //const originalRequest = error.config;
+        /*if (error.response.status === 401 && error.response.statusText === "Unauthorized"){
             const refresh_token = localStorage.getItem('refresh_token');
 
             return axiosInstance.post('/rest-auth/login/', {refresh: refresh_token})
             .then(response => {
-                localStorage.setItem('access_token', response.data.key);
+                localStorage.setItem('access_key', response.data.key);
                 localStorage.setItem('refresh_token', response.data.refresh);
 
-                axiosInstance.defaults['Authorization'] = "JWT" + response.data.access;
-                originalRequest.headers['Authorization'] = "JWT" + response.data.access;
+                axiosInstance.defaults['Authorization'] = "JWT" + response.data.key;
+                originalRequest.headers['Authorization'] = "JWT" + response.data.key;
 
                 return axiosInstance(originalRequest);
             })
             .catch(err => console.error(err));
-        }
-        return Promise.reject(error)
+        }*/
+        //return Promise.reject(error)
     }
-    )
-
-    export default axiosInstance;
+);
